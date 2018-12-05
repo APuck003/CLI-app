2c4fa797f5a27a0b7da07034f49a87d5b4051c52
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2018-12-05 13:42:53 -0600

    fixed apostrophe issue

--- send_text.rb
+++ send_text.rb
@@ -1,13 +1,12 @@
-# osascript -e 'tell application "Messages" to send "Hello John!" to buddy "John Doe"'
-
 def get_text
   puts "Enter full name of contact: "
   name = gets.chomp
   
   puts "Enter message you want to send: "
-  message = gets.chomp
-  
-  # term_command = "osascript -e 'tell application \"Messages\" to send \"#{message}\" to buddy \"#{name}\"'"
+  # .gsub!("'", '\'')
+  message = gets.chomp.gsub("'", "`")
+
+  # If message variable has single or double quotes, the command won't execute in command line
   %x(osascript -e 'tell application "Messages" to send "#{message}" to buddy "#{name}"')
 end
 
@@ -16,3 +15,6 @@ def send_text
 end
 
 send_text
+
+
+# %x(osascript -e 'tell application "Messages" to send "I'm a "teapot."" to buddy "Ryan Daley"')
