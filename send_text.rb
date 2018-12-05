d33b8882049bbfe4abf98bb53aa30c22753e6b43
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2018-12-05 13:44:15 -0600

    updated code

--- send_text.rb
+++ send_text.rb
@@ -3,10 +3,8 @@ def get_text
   name = gets.chomp
   
   puts "Enter message you want to send: "
-  # .gsub!("'", '\'')
   message = gets.chomp.gsub("'", "`")
 
-  # If message variable has single or double quotes, the command won't execute in command line
   %x(osascript -e 'tell application "Messages" to send "#{message}" to buddy "#{name}"')
 end
 
@@ -15,6 +13,3 @@ def send_text
 end
 
 send_text
-
-
-# %x(osascript -e 'tell application "Messages" to send "I'm a "teapot."" to buddy "Ryan Daley"')
