32d05e2374fa1dae1e42654ff9ded3ddcdb3df16
rdaley321 <rdaley321@yahoo.com>
2018-12-07 12:17:53 -0600

    Fixed rake

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,8 @@
 require 'bundler'
 Bundler.require
 
+
+
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
 require_all 'models'
-require 'artii'
-require 'lolcat'
-require_relative '../send_text'
